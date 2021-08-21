9aab9ef1fd52269c4a398f8db191952105399277
brian <briancalabuig@gmail.com>
2021-08-21 20:13:42 +0200

    Update library.js

--- library.js
+++ library.js
@@ -4,4 +4,4 @@ var webdriver = require('selenium-webdriver'),
 
   var driver = new webdriver.Builder().forBrowser('chrome').build();
 
-  driver.get('https://google.es')
+  driver.get('https://www.marca.com/')
